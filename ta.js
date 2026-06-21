b341fcaa434ad1a1abfd02c884e33a93a19bd44a
abby_baldinger <abalding@purdue.edu>
2026-06-21 17:49:33 -0400

    Worked on UI for TA FAQ Posts, need to fix bug with remove when theres only one element and make the side navigation collapsable, also need to add graphs for the wait time and number of students in line to home

--- ta.js
+++ ta.js
@@ -23,4 +23,43 @@ document.addEventListener("DOMContentLoaded", function () {
     settingsButton.addEventListener("click", function () {
         window.location.href = "taSettings.html";
     });
+
+    faqInput = document.getElementById("faqInput");
+    postBtn = document.getElementById("postBtn");
+    recentFaqContainer = document.querySelector(".recent-faq-container");
+    if (postBtn && faqInput && recentFaqContainer) {
+        postBtn.addEventListener("click", function () {
+            textValue = faqInput.value.trim();
+            if (textValue === "") {
+                alert("Please type a message before posting!");
+                return;
+            }
+            newFaqItem = document.createElement("div");
+            newFaqItem.className = "faq-item";
+            newFaqItem.innerHTML = `
+                <div class="faq-content-skeleton">
+                    <p style="font-family: 'Times New Roman', Times, serif; font-size: 1.1rem; margin: 0; text-align: left;">
+                        ${textValue}
+                    </p>
+                </div>
+                <button class="remove-btn-static">REMOVE</button>
+            `;
+            removeBtn = newFaqItem.querySelector(".remove-btn-static");
+            removeBtn.addEventListener("click", function () {
+                newFaqItem.remove();
+            });
+            recentFaqContainer.prepend(newFaqItem);
+            faqInput.value = "";
+        });
+    }
+    existingRemoveButtons = document.querySelectorAll(".remove-btn-static");
+    existingRemoveButtons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            rowItem = event.target.closest(".faq-item");
+            if (rowItem) {
+                rowItem.remove();
+            }
+        });
+    });
 });
+
